abccb80de92c64002255e2516fd5b4099e881375
Richard <richard.trow.davis@gmail.com>
2019-03-14 18:39:25 -0700

    Added errs that dispay if registration is incorrect or throws an error. Added new logout and login modals that authenticate the user in the database. Added modals to nav bar

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -32,6 +32,38 @@ export const loadUser = () => (dispatch, getState) => {
     })
 }
 
+//Register user
+export const register = ({ name, email, password }) => dispatch => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+
+  //Request body
+  const body = JSON.stringify({ name, email, password })
+
+  axios.post('/users', body, config)
+    .then(res => dispatch({
+      type: REGISTER_SUCCESS,
+      payload: res.data
+    }))
+    //For an error with registration
+    .catch(err => {
+      dispatch(returnErrors(err.response.data, err.response.status, 'REGISTER_FAIL'))
+      dispatch({
+        type: REGISTER_FAIL
+      })
+    })
+}
+
+//Logout User
+export const logout = () => {
+  return {
+    type: LOGOUT_SUCCESS
+  }
+}
+
 // Setup config, headers, and token
 export const tokenConfig = getState => {
   //Get token from local storage
